59fbe07f71e224fd1c81c2b1ffbe191f18900211
SufyanFarooq <sufyanmaviya400@gmail.com>
2023-08-20 16:24:00 +0500

    add api and google map

--- src/Components/Contact/ContactForm.jsx
+++ src/Components/Contact/ContactForm.jsx
@@ -1,87 +1,150 @@
-import React from 'react'
+import React, { useState } from 'react'
 import phone from '../../assets/phone.png'
 import fax from '../../assets/fax.png'
 import email from '../../assets/email.png'
+import 'react-phone-number-input/style.css'
+import { useForm, Controller } from "react-hook-form";
+import PhoneInput from 'react-phone-number-input';
+import axios from 'axios'
 const ContactForm = () => {
+  const { register, handleSubmit, watch, control, formState: { errors } } = useForm();
+  const [isLoading, setIsLoading] = useState(false)
+  const [btnText, setBtnText] = useState("Submit Now")
+  const onSubmit = async (data) => {
+    try {
+      setBtnText("Waiting...")
+      await axios.post("https://server.triviummbs.com/api/v1/sendEmailtoUser", data)
+      setBtnText("Your request submited")
+      setIsLoading(true)
+    } catch (error) {
+      setIsLoading(false)
+      console.error("error while on submit", error);
+    }
+  };
   return (
     <div className='container pt-5'>
       <div className='row'>
-      <div className='col-lg-6 col-12 '>
-                <h2 className='clr'>Tell Us Where to Reach You</h2>
-                <p className='mb-0'>A member of our team will get back to you within 12 hours.</p>
-                <p>For immediate service, give us a call at 800-640-6409.</p>
-                <div className=''>
-                <div className='demo_box2 justify-content-between d-lg-flex d-block'>
-                    <div className=''>
-                    <label for="exampleInputEmail1" class="form-label">Name</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    </div>
-                    <div className=''>
-                    <label for="exampleInputEmail1" class="form-label">
-Business Name</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    </div>
-                </div>
-                <div className='demo_box2 mt-3 justify-content-around d-lg-block  d-block'>
-                    <div className=''>
-                    <label for="exampleInputEmail1" class="form-label">Email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    </div>
-                    <div className=''>
-                    <label for="exampleInputEmail1" class="form-label">
-Phone</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    </div>
-                </div>
-                <div class="mb-3 demo_box2 mt-3 justify-content-around px-lg-0 px-0">
-  <label for="exampleFormControlTextarea1" class="form-label">Your Message
-</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
-<div className='text-center'>
+        <div className='col-lg-6 col-12 '>
+          <h2 className='clr'>Tell Us Where to Reach You</h2>
+          <p className='mb-0'>A member of our team will get back to you within 12 hours.</p>
+          <p>For immediate service, give us a call at 800-640-6409.</p>
+          <form className='' onSubmit={handleSubmit(onSubmit)}>
+            <div className='demo_box2 justify-content-between d-lg-flex d-block w-100 '>
+              <div className='w-50'>
+                <label for="exampleInputEmail1" class="form-label">Name</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                  {...register("name", { required: true })}
+                />
+                {errors.name && <span className='text-danger'>Name is required</span>}
+              </div>
+              <div className='w-50 ms-5'>
+                <label for="exampleInputEmail1" class="form-label">
+                  Business Name</label>
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                  {...register("businessName", { required: true })}
+                />
+                {errors.businessName && <span className='text-danger'>Business Name is required</span>}
+              </div>
+            </div>
+            <div className='demo_box2 mt-3 justify-content-around d-lg-block  d-block'>
+              <div className=''>
+                <label for="exampleInputEmail1" class="form-label">Email</label>
+                <input type="email" class="form-control" aria-describedby="emailHelp"
+                  id="email"
+                  {...register("email", {
+                    required: "Email is required",
+                    pattern: {
+                      value: /\S+@\S+\.\S+/,
+                      message: "Entered value does not match email format"
+                    }
+                  })}
+                />
+                {errors.email && <span className='text-danger' role="alert">{errors.email.message}</span>}
+              </div>
+              <div className=''>
+                <label for="exampleInputEmail1" class="form-label">
+                  Phone</label>
+                <Controller
+                  name="phoneNumber"
+                  control={control}
+                  rules={{ required: 'Phone number is required' }}
+                  render={({ field }) => <PhoneInput
+                    class="form-control "
+                    defaultCountry="US"
+                    international
+                    countryCallingCodeEditable={false}
+                    {...field}
+                  />}
+                />
 
-<button className='btn btn-primary w-100 p-2 text-white px-4'>Submit Now</button>
-</div>
-                </div>
-            
-                </div>
-                <div className='col-lg-6 col-12'>
-<h2 className='clr'>ADDRESS</h2>
-<h2>312 W 2nd Street Unit A779 Casper, Wy 82601</h2>
-{/* <p>150 Washington Avenue Suite #201, Santa Fe New Mexico 87501</p>
-<h2>California Office</h2>
-<p>18001 Sky Park Cir Suite R Irvine, CA 92614
+                {errors.phoneNumber && <span className='text-danger'>{errors.phoneNumber.message}</span>}
+              </div>
+            </div>
+            <div class="mb-3 demo_box2 mt-3 justify-content-around px-lg-0 px-0">
+              <label for="exampleFormControlTextarea1" class="form-label">Your Message
+              </label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                {...register("description", { required: true })}
+              ></textarea>
+              {errors.description && <span className='text-danger'>Description is required</span>}
+            </div>
+            <div className='text-center'>
 
-</p>
-<h2>Find Us on Map</h2> */}
-                </div>
+              <button className='btn btn-primary w-100 p-2 text-white px-4' disabled={isLoading} type='submit'>{btnText}</button>
+            </div>
+          </form>
+
+        </div>
+        <div className='col-lg-6 col-12'>
+          <h2 className='clr'>Address</h2>
+          <h2>312 W 2nd St a779, Casper, WY 82601, USA</h2>
+          <div>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2925.1083417055265!2d-106.33164712349117!3d42.84944547115163!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8760bca480ca44a5%3A0xa22e368b60645bd9!2s312%20W%202nd%20St%20a779%2C%20Casper%2C%20WY%2082601%2C%20USA!5e0!3m2!1sen!2s"
+              width="100%"
+              height="400"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              title="Embedded Content"
+            ></iframe>
+
+            {/* <iframe
+              src="https://goo.gl/maps/HeFJwJiNTY3MzPHH8"
+              width="100%"
+              height="400"
+              title="Embedded Content"
+              allowFullScreen
+            ></iframe> */}
+          </div>
+        </div>
       </div>
       <div className='row mt-5'>
         <div className='col-lg-12 d-lg-flex d-block gap-3'>
-            <div className='col-lg-3 revenue_box text-center'>
-                <img src={phone} alt="" className='img-fluid mb-3'/>
-                <p>Phone</p>
-                <p>+1 800 640 6409</p>
+          <div className='col-lg-3 revenue_box text-center'>
+            <img src={phone} alt="" className='img-fluid mb-3' />
+            <p>Phone</p>
+            <p>+1 800 640 6409</p>
 
-            </div>
-            <div className='col-lg-3 revenue_box text-center'>
-                <img src={fax} alt="" className='img-fluid mb-3'/>
-                <p>Fax</p>
-                <p>+1 800 640 6409</p>
+          </div>
+          <div className='col-lg-3 revenue_box text-center'>
+            <img src={fax} alt="" className='img-fluid mb-3' />
+            <p>Fax</p>
+            <p>+1 800 640 6409</p>
 
-            </div>
-            <div className='col-lg-3 revenue_box text-center'>
-                <img src={email} alt="" className='img-fluid mb-3'/>
-                <p>For Sales</p>
-                <p>+1 800 640 6409</p>
+          </div>
+          <div className='col-lg-3 revenue_box text-center'>
+            <img src={email} alt="" className='img-fluid mb-3' />
+            <p>For Sales</p>
+            <p>+1 800 640 6409</p>
 
-            </div>
-            <div className='col-lg-3 revenue_box text-center'>
-                <img src={email} alt="" className='img-fluid mb-3'/>
-                <p>For Info</p>
-                <p>+1 800 640 6409</p>
+          </div>
+          <div className='col-lg-3 revenue_box text-center'>
+            <img src={email} alt="" className='img-fluid mb-3' />
+            <p>For Info</p>
+            <p>+1 800 640 6409</p>
 
-            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,101 +2,95 @@ import React from 'react'
 import './footer.css'
 import { NavLink } from 'react-router-dom'
 const Footer = () => {
-  return (
-    <div className='footer mt-3 p-5'>
-  <div className='container pt-5 '>
-        <div className='row'>
-   <div className='col-lg-3 col-12'>
-<h5 className='text-white'>Our Services</h5>
-<ul className='p-0 footer_ul'>
-   <NavLink to="medical">
-   <li>Medical Billing</li>
-    </NavLink> 
-    <NavLink to="laboratory">
-    <li>Laboratory Billing</li>
-    </NavLink>
-    <NavLink to="surgery">
-    <li>Ambulatory Surgery</li>
-    </NavLink>
-    <NavLink to="recovery">
-    <li>AR Recovery Services</li>
-    </NavLink>
-    <NavLink to="imaging">
-    <li>Imaging Billing</li>
-    </NavLink>
-    <NavLink to="hospital">
-    <li>Hospital Billing</li>
-    </NavLink>
-    </ul>
-   </div>
-   <div className='col-lg-3 col-12'>
-<h5 className='text-white'>Specialties</h5>
-<ul className='p-0 footer_ul'>
-<NavLink to="/sepcial/hepatology">
-<li>Hepatology
-</li>
-</NavLink>
-<NavLink to="/sepcial/oncology">
-<li>Oncology
-</li>
-</NavLink>
+   return (
+      <div className='footer mt-3 p-5'>
+         <div className='container pt-5 '>
+            <div className='row'>
+               <div className='col-lg-3 col-12'>
+                  <h5 className='text-white'>Our Services</h5>
+                  <ul className='p-0 footer_ul'>
+                     <NavLink to="medical">
+                        <li>Medical Billing</li>
+                     </NavLink>
+                     <NavLink to="laboratory">
+                        <li>Laboratory Billing</li>
+                     </NavLink>
+                     <NavLink to="surgery">
+                        <li>Ambulatory Surgery</li>
+                     </NavLink>
+                     <NavLink to="recovery">
+                        <li>AR Recovery Services</li>
+                     </NavLink>
+                     <NavLink to="imaging">
+                        <li>Imaging Billing</li>
+                     </NavLink>
+                     <NavLink to="hospital">
+                        <li>Hospital Billing</li>
+                     </NavLink>
+                  </ul>
+               </div>
+               <div className='col-lg-3 col-12'>
+                  <h5 className='text-white'>Specialties</h5>
+                  <ul className='p-0 footer_ul'>
+                     <NavLink to="/sepcial/hepatology">
+                        <li>Hepatology
+                        </li>
+                     </NavLink>
+                     <NavLink to="/sepcial/oncology">
+                        <li>Oncology
+                        </li>
+                     </NavLink>
 
-<NavLink to="/sepcial/psychiatric">
-<li>Psychiatric</li>
-</NavLink>
-<NavLink to="/sepcial/urology">
-<li>Urology</li>
-</NavLink>
-<NavLink to="/sepcial/dermatology">
-<li>Dermatology</li>
-</NavLink>
-<NavLink to="/sepcial/cardiology">
+                     <NavLink to="/sepcial/psychiatric">
+                        <li>Psychiatric</li>
+                     </NavLink>
+                     <NavLink to="/sepcial/urology">
+                        <li>Urology</li>
+                     </NavLink>
+                     <NavLink to="/sepcial/dermatology">
+                        <li>Dermatology</li>
+                     </NavLink>
+                     <NavLink to="/sepcial/cardiology">
 
-   
-<li>Cardiology</li>
-</NavLink>
-  
-   
-  
-</ul>
-   </div>
-   <div className='col-lg-3 col-12'>
-<h5 className='text-white'>Quick Links</h5>
-<ul className='p-0 footer_ul'>
-<NavLink to="/">
-<li>Home</li>
-</NavLink>
-<NavLink to="about">
-<li>About Us</li>
-</NavLink>
-<NavLink to="contact">
-<li>Contact Us</li>
-</NavLink>
+
+                        <li>Cardiology</li>
+                     </NavLink>
 
 
 
-   
-    
-    
-   
-</ul>
-   </div>
-   <div className='col-lg-3 col-12'>
-<h5 className='text-white'>Keep Connected
+                  </ul>
+               </div>
+               <div className='col-lg-3 col-12'>
+                  <h5 className='text-white'>Quick Links</h5>
+                  <ul className='p-0 footer_ul'>
+                     <NavLink to="/">
+                        <li>Home</li>
+                     </NavLink>
+                     <NavLink to="about">
+                        <li>About Us</li>
+                     </NavLink>
+                     <NavLink to="contact">
+                        <li>Contact Us</li>
+                     </NavLink>
+                  </ul>
+               </div>
+               <div className='col-lg-3 col-12'>
+                  <h5 className='text-white'>Keep Connected
 
-</h5>
-<ul className='p-0 footer_ul'>
-    <input type="text" className='bg-transparent w-100 border border-light rounded text-white p-2' placeholder='Name '/>
-    <input type="text" className='bg-transparent w-100 border border-light rounded text-white p-2 mt-3' placeholder='Your Email '/>
-    <button className='btn mt-3 btn_demo w-100 text-white'>Subscribe</button>
-</ul>
-   </div>
-        </div>
-      
-    </div>
-    </div>
-  
-  )
+                  </h5>
+                  <ul className='p-0 footer_ul'>
+                     <input type="text" className='bg-transparent w-100 border border-light rounded text-white p-2' placeholder='Name ' />
+                     <input type="text" className='bg-transparent w-100 border border-light rounded text-white p-2 mt-3' placeholder='Your Email ' />
+                     <button className='btn mt-3 btn_demo w-100 text-white'>Subscribe</button>
+                  </ul>
+               </div>
+               
+            </div>
+
+         </div>
+      </div>
+
+   )
 }
 
 export default Footer
